9c953c9ee50ee970d61ff77f75546624fd7b31fd
jonilsonds9 <jonilsonds9@gmail.com>
2020-05-27 13:36:52 -0300

    Express em ação

--- server.js
+++ server.js
@@ -1,36 +1,75 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const servidor = http.createServer(function(req, resp) {
-
-  let html = '';
-  if (req.url == '/') {
-    html = `
-    <!DOCTYPE html>
-    <head>
-      <meta charset="UTF-8">
-      <title>Document</title>
-    </head>
-    <body>
-      <h1> Casa do Código </h1>
-    </body>
-    </html>
-  `;
-  } else if (req.url == '/livros') {
-    html = `
-    <!DOCTYPE html>
-    <head>
-      <meta charset="UTF-8">
-      <title>Document</title>
-    </head>
-    <body>
-      <h1> Listagem de livros </h1>
-    </body>
-    </html>
-  `
-  }
-
-  // outros else if
-
-  resp.end(html);
+app.listen(3000, function() {
+  console.log(`Servidor rodando na porta 3000`);
 });
-servidor.listen(3000);
+
+app.get('/', function(req, res) {
+  res.send(
+    `
+      <!DOCTYPE html>
+      <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+      </head>
+      <body>
+        <h1> Casa do Código </h1>
+      </body>
+      </html>
+    `
+  );
+});
+
+app.get('/livros', function(req, res) {
+  res.send(
+    `
+      <!DOCTYPE html>
+      <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+      </head>
+      <body>
+        <h1> Listagem de livros </h1>
+      </body>
+      </html>
+    `
+  );
+});
+
+// const http = require('http');
+
+// const servidor = http.createServer(function(req, resp) {
+
+//   let html = '';
+//   if (req.url == '/') {
+  //   html = `
+  //   <!DOCTYPE html>
+  //   <head>
+  //     <meta charset="UTF-8">
+  //     <title>Document</title>
+  //   </head>
+  //   <body>
+  //     <h1> Casa do Código </h1>
+  //   </body>
+  //   </html>
+  // `;
+//   } else if (req.url == '/livros') {
+//     html = `
+//     <!DOCTYPE html>
+//     <head>
+//       <meta charset="UTF-8">
+//       <title>Document</title>
+//     </head>
+//     <body>
+//       <h1> Listagem de livros </h1>
+//     </body>
+//     </html>
+//   `
+//   }
+
+//   // outros else if
+
+//   resp.end(html);
+// });
+// servidor.listen(3000);
