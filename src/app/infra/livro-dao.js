8699fa5a74ced8da6c386235c8c291f147fd87ef
jonilsonds9 <jonilsonds9@gmail.com>
2020-05-28 12:05:41 -0300

    DAO com Promises

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -3,12 +3,18 @@ class livroDAO {
     this._db = db;
   }
 
-  lista(callback) {
-    this._db.all(
-      'SELECT * FROM livros',
-      (erro, resultados) => 
-        callback(erro, resultados)
-    )
+  lista() {
+    return new Promise((resolve, reject) => {
+      this._db.all(
+        'SELECT * FROM livros',
+        (erro, resultados) => {
+          if (erro) return reject('Não foi possível listar os livros!');
+
+          return resolve(resultados);
+        }
+      )
+    });
+    
   }
 }
 
--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -21,26 +21,14 @@ module.exports = (app) => {
   app.get('/livros', function(req, res) {
 
     const livroDAO = new LivroDAO(db);
-    
-    livroDAO.lista(function(erro, resultados) {
-
-      res.marko(
-        require('../views/livros/lista/lista.marko'),
-        {
-          livros: resultados
-        }
-      );
-    });
-
-
-    // db.all('SELECT * FROM livros', function(erro, resultados) {
-    //   res.marko(
-    //     require('../views/livros/lista/lista.marko'),
-    //     {
-    //       livros: resultados
-    //     }
-    //   );
-    // });
+    livroDAO.lista()
+            .then(livros => res.marko(
+              require('../views/livros/lista/lista.marko'),
+              {
+                livros: livros
+              }
+            ))
+            .catch(erro => console.log(erro));
 
   });
 };
