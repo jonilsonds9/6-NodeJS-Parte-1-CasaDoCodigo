75521b842f0b6200577b6768d2e0b43a6f37102f
jonilsonds9 <jonilsonds9@gmail.com>
2020-05-28 11:00:26 -0300

    Acessando o Banco de dados sqlite versao 4.2.0

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,3 +1,5 @@
+const db = require('../../config/database');
+
 module.exports = (app) => {
   app.get('/', function(req, res) {
     res.send(
@@ -16,20 +18,14 @@ module.exports = (app) => {
   });
   
   app.get('/livros', function(req, res) {
-    res.marko(
-      require('../views/livros/lista/lista.marko'),
-      {
-        livros: [
-          {
-            id: 1,
-            titulo: 'Fundamentos do Node'
-          },
-          {
-            id: 2,
-            titulo: 'Node avançado'
-          }
-        ]
-      }
-    );
+    db.all('SELECT * FROM livros', function(erro, resultados) {
+
+      res.marko(
+        require('../views/livros/lista/lista.marko'),
+        {
+          livros: resultados
+        }
+      );
+    });
   });
 };
